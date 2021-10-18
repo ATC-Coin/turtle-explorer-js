97857da657d14e45be18db77eb8d6c5ddf5110a0
maletazul <luissilva@maletazul.pt>
2021-10-18 12:14:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://148.251.194.145:22898';
+var api = 'http://148.251.194.145:8070';
 var donationAddress = "";
 var blockTargetInterval = 30;
 var coinUnits = 100;
@@ -6,14 +6,14 @@ var symbol = 'atsc';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
- "trtl": [
+ "atsc": [
 	["pool.atc-coin.com", "http://pool.atc-coin.com:8117"],
 	["xk.is", "https://xk.is/api"]
  ]
 };
 
 var networkStat2 = {
-    "trtl": [
+    "atsc": [
 	[""]
  ]
 };
